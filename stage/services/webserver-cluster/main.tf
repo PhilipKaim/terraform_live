ac31e3538aad6cd09fb0f48d65fd9913c4d8f57a
Chillup <philipkaim@gmail.com>
2021-03-10 19:08:58 -0500

    update source to use modules repository

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "webserver_cluster" {
-  source = "../../../modules/services/webserver-cluster"
+  source = "github.com/PhilipKaim/terraform_modules//services/webserver-cluster?ref=v0.0.1"
 
   cluster_name           = "webservers-stage"
   db_remote_state_bucket = "terraform-up-and-running-state-pk"
